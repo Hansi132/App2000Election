9061e6a8481ffada1f48adfd34a1b7e7f80bf580
lukmasipraba <59934514+lukmasipraba@users.noreply.github.com>
2021-06-03 16:04:52 +0200

    fix

--- electionbackend/src/main/resources/static/portalApp/navbarDesktop.js
+++ electionbackend/src/main/resources/static/portalApp/navbarDesktop.js
@@ -2,7 +2,7 @@ function openNav() {
     document.getElementById("mySidepanel").style.width = "350px";
 }
 
-/* Set the width of the sidebar to 0 (hide it) */
+
 function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
 }
